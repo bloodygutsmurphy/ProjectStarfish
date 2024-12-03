8cc4114193dbfb071f96d7c63b75b141e26d9d7f
bloodygutsmurphy <bleedingutsmurphy@proton.me>
2024-12-03 23:59:20 +0000

    Safety Commit
    Due to a failed reparenting of a handful of actors, the project became corrupted and would crash Windows. This commit addresses that issue.

--- BoomerShooter/Intermediate/Build/Win64/BoomerShooterEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++ BoomerShooter/Intermediate/Build/Win64/BoomerShooterEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -1,2 +1,2 @@
 // Compiler: 14.38.33133
-#include "C:/Users/TheAm/Documents/_Archive/Unreal Projects/BoomerShooterTemplate - Broken/Intermediate/Build/Win64/BoomerShooterEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
+#include "C:/Users/TheAm/Downloads/ProjectStarfish-main/BoomerShooter/Intermediate/Build/Win64/BoomerShooterEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
--- BoomerShooter/Source/BoomerShooter/BoomerShooter.Build.cs
+++ BoomerShooter/Source/BoomerShooter/BoomerShooter.Build.cs
@@ -8,7 +8,7 @@ public class BoomerShooter : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "BlockoutToolsPlugin", "BlockoutToolsEditorPlugin" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
